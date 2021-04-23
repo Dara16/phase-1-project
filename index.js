e5b0b902707a5b0fba77aba78d100ed51f7eae03
dara <just4oludee@yahoo.com>
2021-04-22 21:42:32 -0400

    date function created

---
+++ index.js
@@ -0,0 +1,11 @@
+function dateDisplay(){
+    var today=new Date()
+    var dd=today.getDate()
+    var mm=today.getMonth()+1
+    var yy=today.getFullYear()
+    if(dd<10) dd='0'+dd;
+    if(mm<10) mm='0'+mm;
+    
+    return (mm+"/"+dd+"/"+yy);  
+    }
+    console.log(dateDisplay())
